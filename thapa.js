9298b865b1ddf4a10e15dc600a9c4f55d96a22c9
PRATIK-PHALE <pratikphale6872@gmail.com>
2022-12-20 17:15:41 +0530

    add all file

---
+++ thapa.js
@@ -0,0 +1,21 @@
+10+"20"
+console.log(10+"20");
+9-"5"
+console.log(9 -"5"); //bug
+
+"java"+"script"
+console.log("java "+" script");
+""+""
+console.log(""+"");
+""+0
+console.log(""+0);
+"vinod"+"thapa"
+console.log("vinod"+"thapa");
+true+true
+console.log(true+true);
+true+false
+console.log(true+false);
+false+true
+console.log(false+true);
+false-true
+console.log(false-true);
