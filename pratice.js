7c17859b632be9b02dfb3e6289a47aa2b30cbf36
PRATIK-PHALE <pratikphale6872@gmail.com>
2022-12-20 17:34:57 +0530

    update name

---
+++ pratice.js
@@ -0,0 +1,42 @@
+10+"20"
+console.log(10+"20");
+9-"5"
+console.log(9 -"5"); //bug
+
+"java"+"script"
+console.log("java "+" script");
+""+""
+console.log(""+"");
+""+0
+console.log(""+0);
+"vinod"+"thapa"
+console.log("vinod"+"thapa");
+console.log("vinod"-"thapa");
+
+true+true
+console.log(true+true);
+true+false
+console.log(true+false);
+false+true
+console.log(false+true);
+false-true
+console.log(false-true);
+
+// Interview Question-------
+// Different between null vs undefiend--------------
+
+var useless= null;
+console.log(useless);                               
+console.log(typeof(useless));
+
+
+var imstandby;
+console.log(imstandby);
+console.log(typeof(imstandby));
+
+// what is Nan?----------
+
+// -it is a property of the global object
+// - in other words, it is a variable in global scope
+// -The initial value of NaN is Not-a-number.
+
